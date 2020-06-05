fa911c0b7de36c75f8d39e551babe4603695cacd
MathieuBoe <mathieuboe60@gmail.com>
2020-06-05 12:25:29 +0200

    Exercice 8 complet

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$title = 'Exercice8';
+$isOK = Null;
+include 'header.php';
+if ($isOK== Null)
+{
+    echo "C'est ok !!!";
+}
+else 
+{
+    echo "C'est pas bon !!!";
+}
+include 'footer.php';?>
